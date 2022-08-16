ed6310880f2b9b917e8b0fa2e3cc8e0127d5bfe8
Adhawale10feb <adhawale10feb@gmail.com>
2022-08-16 09:57:29 +0530

    New scroll function added in comman class and new comments added in same

--- src/main/java/Base_utility/Comman.java
+++ src/main/java/Base_utility/Comman.java
@@ -4,6 +4,7 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.io.File;
@@ -12,6 +13,7 @@ import java.time.Duration;
 
 public class Comman {
     public static WebDriver driver;
+
     // Method to invoke browser by name
     public void InvokeBrowser(String BrowserName)
     {
@@ -40,41 +42,66 @@ public class Comman {
         wait.until(ExpectedConditions.visibilityOf(Element));
         Element.click(); }
 
+    //This function is used to enter the value in specified webelement
     public void EnterValue(WebElement Element, String Value)
     {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(40));
         wait.until(ExpectedConditions.visibilityOf(Element));
         Element.sendKeys(Value);
     }
+
+    //This function is used to load the URL using navigate command
     public void LoadURL(String Url)
     {
         driver.navigate().to(Url);
     }
 
+    //This function is used to set the implicite wait
     public void ImpliWait(Long Time)
     {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(Time));
     }
 
+    //This function is used to maximize the browser
     public void MaximiseBrowser() {
         driver.manage().window().maximize();
     }
 
+    //This function is used to set the explicite wait  untill element visibility for  specified time
     public  void expwait(WebElement Element, long Time)
     {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(Time));
         wait.until(ExpectedConditions.visibilityOf(Element));
     }
+
+    //This function is used to quit the browser with 5 sec wait
     public void Quit()
+    {try {Thread.sleep(5000);} catch(Exception e) {e.printStackTrace();}
+        driver.quit();}
+
+    // this function is used to hover mounse on a webelement
+    public void HoverMouseOn(WebElement Element)
     {
-        try
-        {
-            Thread.sleep(5000);
-        }
-        catch(Exception e)
-        {
-            e.printStackTrace();
-        }
-        driver.quit();
+        Actions action= new Actions(driver);
+        action.moveToElement(Element);
     }
+
+    // This function is used to right-click on a web element
+    public void RightClickOn(WebElement Element)
+    {
+        Actions action= new Actions(driver);
+        action.contextClick(Element);
+    }
+
+
+    // This function is used to scroll upto certain webElement
+    public void ScrollTo(WebElement Element)
+    {
+        JavascriptExecutor js= (JavascriptExecutor) driver;
+        js.executeScript("argument[0].srollIntoView(true), Element");
+    }
+
+
+
+
 }
--- src/main/java/Page_Utility/SourcingPage.java
+++ src/main/java/Page_Utility/SourcingPage.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.support.PageFactory;
 
 public class SourcingPage extends Comman
 {
+    Comman comman= new Comman();
     public SourcingPage(WebDriver driver)
     {
         this.driver=driver;
@@ -16,5 +17,34 @@ public class SourcingPage extends Comman
 
     @FindBy(xpath = "//span[@class='mat-content']/mat-panel-title")
     public WebElement Header;
+
+    @FindBy(xpath = "//input[@id='mat-input-0']")
+    public WebElement MobileNo;
+
+    @FindBy(xpath = "//input[@id='mat-input-1")
+    public WebElement PanNo;
+
+    @FindBy(xpath = "//input[@id='mat-input-2")
+    public WebElement RequestedAmount;
+
+    @FindBy(xpath = "//span[text()='Continue']")
+    public WebElement Continue;
+
+    @FindBy(xpath = "//div[@class='logout']/button[@type='button']")
+    public WebElement Logout;
+
+
+    public void ClickOnRequestedAmount(String value)
+    {
+        comman.EnterValue(RequestedAmount,value);
+    }
+    public void ClickOnContinue()
+    {
+        comman.ClickOn(Continue);
+    }
+    public void ClickOnLogout()
+    {
+        comman.ClickOn(Logout);
+    }
 
 }
